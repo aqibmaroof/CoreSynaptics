4e7dae551f2862effdf9225660afdd18baa1fe64
Lucifer <aqibmaroof786@gmail.com>
2026-03-09 04:17:28 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  images: {
-    domains: [
-      "wildtag-s3-bucket.s3.eu-north-1.amazonaws.com",
-    ],
-  },
+
 };
 
 export default nextConfig;
